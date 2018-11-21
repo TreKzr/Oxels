eede14757fbb67ee8821aafd12b903a5f242bcf6
TreKzr <45246402+TreKzr@users.noreply.github.com>
2018-11-21 23:11:32 +0000

    Add files via upload

---
+++ Oxels.py
@@ -0,0 +1,28 @@
+
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+@asyncio.coroutine on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+@asyncio.coroutine on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run('NTE0MjA3NzI3NjI3NTM0MzUw.DtVsOQ.GADgu9q1pMeVKJGel455HpH6KgM')
